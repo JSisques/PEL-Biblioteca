e4acd82c1afe046d21076c9aaca6e211ab49a033
JSisques <javierplazasisques@gmail.com>
2021-10-01 23:59:26 +0200

    Metodos existeDirectorio()y guardarJSON hechos

--- Fichero.hpp
+++ Fichero.hpp
@@ -1,24 +1,58 @@
 #include <string>
-#include <Constants.hpp>
+#include <fstream>
+#include "Constants.hpp"
+#include "Usuario.cpp"
+#include <sys/stat.h>
+
+using namespace std;
 
 class Fichero{
     private:
         string fileName;
         
     public:
-        bool existeDirectorio(string path);
-        bool guardarDatosJSON(Usuario);
-        bool guardarDatosJSON(Biblioteca);
-        Biblioteca leerDatosJSON(string);
-        Usuario leerDatosJSON(string);
+        string guardarJSON(string, string);
+        bool existeDirectorio(string);
+        string crearJSON(Usuario);
+        //bool crearJSON(Biblioteca);
+        //Biblioteca leerDatosJSON(string);
+        //Usuario leerDatosJSON(string);
 };
 
-bool Fichero::guardarDatosJSON(Usuario user){
-
+/*
+Este metodo gardara un string en formato JSON en un fichero indicado por parametro.
+Devuelve el propio JSON pasado por parámetro.
+*/
+string Fichero::guardarJSON(string json, string nombreFichero){
+    ofstream out(Constants::DATA_PATH + nombreFichero + Constants::JSON_EXTENSION);
+    out << json;
+    out.close();
+    return json;
 }
 
-bool Fichero::guardarDatosJSON(Biblioteca biblioteca){
+/*
+Este metodo comprobara si existe el directorio pasado por parametro.
+Devolverá un 1 si existe o un 0 en caso contrario.
+*/
+bool Fichero::existeDirectorio(string path){
+    struct stat buffer;
+    return (stat (path.c_str(), &buffer) == 0);
+}
 
+/*
+Este método creará un JSON a partir de los datos del usuario pasado como parametro.
+Devolverá un String con el JSON creado.
+*/
+string Fichero::crearJSON(Usuario user){
+    string json = "";
+
+    json += "terere";
+
+    return json;
+}
+/*
+bool Fichero::crearJSON(Biblioteca biblioteca){
+    return true;
 }
 
 Biblioteca Fichero::leerDatosJSON(){
@@ -28,3 +62,4 @@ Biblioteca Fichero::leerDatosJSON(){
 Usuario Fichero::leerDatosJSON(string dni){
 
 }
+*/
--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-#include <Fichero.hpp>
+#include "Fichero.hpp"
+#include "Constants.hpp"
 
 using namespace std;
 
 int main() {
 
     Fichero f = Fichero();
-    
 
+    bool response = f.existeDirectorio(Constants::DATA_PATH);
+    cout << response;
+
+    f.guardarJSON("{\"nombre\": \"Javi\"}", "Prueba");
     return 0;
 }
