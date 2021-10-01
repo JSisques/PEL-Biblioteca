e5ffcc707d6430d20998c68552c2b7218dc46f0a
JSisques <javierplazasisques@gmail.com>
2021-10-02 00:34:01 +0200

    Updated

--- Biblioteca.hpp
+++ Biblioteca.hpp
@@ -7,5 +7,14 @@ class Biblioteca{
         Libro libros[20];
         Libro libros[20];
     public:
-        int cuantosEjemplaresHay(0);
+        int cuantosEjemplaresHayDisponibles();
 };
+
+/*
+Este método obtiene cuantos ejemplares hay disponibles en la biblioteca.
+Devolverá el numero de ejemplares disponibles.
+*/
+int Biblioteca::cuantosEjemplaresHayDisponibles(){
+    
+    return 0;
+}
--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include "Fichero.hpp"
 #include "Constants.hpp"
+#include "Usuario.cpp"
 
 using namespace std;
 
 int main() {
 
     Fichero f = Fichero();
+    //Usuario u = Usuario(0, "Prueba", "11111111J");
 
     bool response = f.existeDirectorio(Constants::DATA_PATH);
     cout << response;
 
     f.guardarJSON("{\"nombre\": \"Javi\"}", "Prueba");
+    //f.crearJSON(u);
     return 0;
 }
